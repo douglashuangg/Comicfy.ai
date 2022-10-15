1b9a81fde395f499df60195a1eb77342d3e81164
Addison <74084822+addison-ch@users.noreply.github.com>
2022-10-15 12:08:13 -0400

    add image generation

---
+++ server/image_generation.py
@@ -0,0 +1,64 @@
+import requests
+import json
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+wombo_key = os.getenv('WOMBO_KEY')
+
+BASE_URL = "https://api.luan.tools/api/tasks/"
+HEADERS = {
+    'Authorization': f'Bearer {wombo_key}',
+    'Content-Type': 'application/json'
+}
+
+
+def send_task_to_dream_api(style_id, prompt, target_img_path=None):
+
+    generate_payload = {'use_target_image': 'false'}
+    post_response = requests.post(BASE_URL, json = generate_payload, headers=HEADERS)
+    task_id = post_response.json()["id"]
+    task_url = f"https://api.luan.tools/api/tasks/{task_id}"
+    put_payload = json.dumps({            
+    "input_spec": {                    
+    "style": style_id,                    
+    "prompt": prompt,                    
+    "target_image_weight": 0.1,                    
+    "width": 960,                    
+    "height": 1560    
+    }})
+    requests.request("PUT", task_url, headers=HEADERS, data=put_payload)
+    
+    img_url = ''
+    while True:            
+        response_json = requests.request(                    
+            "GET", task_url, headers=HEADERS).json()     
+        img_url = response_json["result"]    
+        state = response_json["state"]    
+        print(img_url)
+        if state == "completed":                    
+            r = requests.request("GET", response_json["result"])   
+            with open("image.jpg", "wb") as image_file:                            
+                image_file.write(r.content)                        
+            print("image saved successfully :)")                    
+            break 
+    
+        elif state =="failed":                    
+            print("generation failed :(")                    
+            break            
+        time.sleep(3)
+    
+    return img_url
+    
+
+
+prompt = "Rhaenyra returns to King's Landing, prematurely ending an unsuccessful months-long tour to choose a consort."
+
+a = []
+
+for i in range(6):
+    a.append(send_task_to_dream_api(5, prompt))
+
+print(a)
