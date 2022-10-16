682f6e69a3e4da6b1157870d4f0494abdb00ef45
douglashuangg <74084786+douglashuangg@users.noreply.github.com>
2022-10-16 02:33:40 -0400

    change speech to one button

--- client/src/components/Comic/Speech.js
+++ client/src/components/Comic/Speech.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
-// import "../style.css";
 
 export default function Speech(props) {
   const msg = new SpeechSynthesisUtterance();
+  const [play, setPlay] = useState(true);
 
   const speechHandler = (msg) => {
-    msg.text = props.labels.join(". ");
-    window.speechSynthesis.speak(msg);
-  };
-
-  const cancelNarration = (msg) => {
-    window.speechSynthesis.cancel(msg);
+    if (play) {
+      msg.text = props.labels.join(". ");
+      window.speechSynthesis.speak(msg);
+      window.speechSynthesis.resume(msg);
+      setPlay(!play);
+    } else {
+      window.speechSynthesis.pause(msg);
+      console.log(play);
+      setPlay(!play);
+    }
   };
 
   return (
     <div className="App">
-      <h1>React Text to Speech App</h1>
-      <button onClick={() => speechHandler(msg)}>NARRATE</button>
-      <button onClick={() => cancelNarration(msg)}>Cancel Talk</button>
+      <button onClick={() => speechHandler(msg)}>Narrate</button>
     </div>
   );
 }
